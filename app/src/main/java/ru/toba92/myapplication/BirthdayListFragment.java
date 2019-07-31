5a96da2966237131c4b61c8df63a5d1c2bc2a286
Toba61rus <toba92@mail.ru>
2019-07-31 14:28:21 +0300

    Реализовану удаление пользователя из появляющегос диалогового окна(по долгмоу нажатию элемента списка)

--- app/src/main/java/ru/toba92/myapplication/BirthdayListFragment.java
+++ app/src/main/java/ru/toba92/myapplication/BirthdayListFragment.java
@@ -1,10 +1,10 @@
 package ru.toba92.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -17,15 +17,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
+import java.util.UUID;
 
 
 //Контроллер представления списка пользователей.
 public class BirthdayListFragment extends Fragment {
 
 
+    private static final int REQUEST_ID_DELETE =0;
     private RecyclerView mRecyclerViewListBirthday;
     private BirthdayAdapter mBirthdayAdapter;
     private static final String DIALOG_DELETE_USER="DialogDeleteUser";
@@ -119,17 +120,21 @@ public class BirthdayListFragment extends Fragment {
             itemView.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View v) {
-//                    FragmentManager fragmentManager=getFragmentManager();
-//                    FragmentDeleteUser fragmentDeleteUser=new FragmentDeleteUser();
-//                    fragmentDeleteUser.show(fragmentManager,DIALOG_DELETE_USER);
-//
-                    BirthdayLab.get(getActivity()).deleteBirthday(mBirthday);
-                    updateUI();
+
+
+                    FragmentManager fragmentManager=getFragmentManager();
+                    FragmentDeleteUser fragmentDeleteUser=FragmentDeleteUser.newInstance(mBirthday.getId());
+                    fragmentDeleteUser.setTargetFragment(BirthdayListFragment.this,REQUEST_ID_DELETE);
+                    fragmentDeleteUser.show(fragmentManager,DIALOG_DELETE_USER);
+
+//                    BirthdayLab.get(getActivity()).deleteBirthday(mBirthday);
+//                    updateUI();
 
                     return true;
                 }
             });
         }
+
 
         @Override
         public void onClick(View v) {
@@ -168,6 +173,20 @@ public class BirthdayListFragment extends Fragment {
         }
         public void setBirthday(List<Birthday>  birthday){
             mBirthdays=birthday;
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode,int resultCode, Intent idUserDelete){
+        if (resultCode!= Activity.RESULT_OK){
+            return;
+        }
+        if (requestCode==REQUEST_ID_DELETE){
+
+            UUID id=(UUID) idUserDelete.getSerializableExtra(FragmentDeleteUser.EXTRA_ID_USER_DELETE);
+            Birthday birthday=BirthdayLab.get(getActivity()).getBirthday(id);
+            BirthdayLab.get(getActivity()).deleteBirthday(birthday);
+
+            updateUI();
         }
     }
 }
--- app/src/main/java/ru/toba92/myapplication/FragmentDeleteUser.java
+++ app/src/main/java/ru/toba92/myapplication/FragmentDeleteUser.java
@@ -1,24 +1,43 @@
 package ru.toba92.myapplication;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
+
+import java.util.UUID;
 
 public class FragmentDeleteUser extends DialogFragment {
 
+    private static final String ARG_ID_USER_DELETE ="idUserDelete";
+    public static final String EXTRA_ID_USER_DELETE="ru.toba92.myapplication.id_user_delete";
+    private Birthday mUserDelete;
+    private FragmentDeleteUser mFragmentDeleteUser;
 
+//Мето получения арумента ID данного пользователя,для дальнейшего его удаления.
 
+    public static FragmentDeleteUser newInstance(UUID idUserDelete){
+        Bundle arg=new Bundle();
+        arg.putSerializable(ARG_ID_USER_DELETE,idUserDelete);
+
+        FragmentDeleteUser fragmentDeleteUser=new FragmentDeleteUser();
+        fragmentDeleteUser.setArguments(arg);
+        return fragmentDeleteUser;
+
+    }
+//   Cоздание диалогово окна.
     @Override
     public Dialog onCreateDialog(Bundle saveInstanceState){
 
-        return new AlertDialog.Builder(getActivity()).setTitle(R.string.delete_user).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+        return new AlertDialog.Builder(getActivity()).setTitle(R.string.delete_user ).setIcon(R.drawable.ic_warning_black_24dp).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
-
+                UUID id=(UUID)getArguments().getSerializable(ARG_ID_USER_DELETE);//Получения ID из аргумента переданного из BirthdayListFragment, и перердачи его в виде интента при нажатии кнопки ОК.
+                senResult(Activity.RESULT_OK,id);
             }
         }).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
             @Override
@@ -27,5 +46,15 @@ public class FragmentDeleteUser extends DialogFragment {
             }
         }).create();
 
+    }
+//   Метод создания интента и заворачивании его с кодом проверки для передачи целевому фрагменту.
+    private void  senResult(int resultCode,UUID idUserDelete){
+        if (getTargetFragment()==null){
+            return;
+        }
+        Intent intent=new Intent();
+        intent.putExtra(EXTRA_ID_USER_DELETE,idUserDelete);
+
+        getTargetFragment().onActivityResult(getTargetRequestCode(),resultCode,intent);
     }
 }
